350512c79ff68d7faff03f852f639b1c5e49dd59
Alex <alexeusgr@gmail.com>
2023-10-07 16:51:00 +0300

    test readFloat

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,9 +1,15 @@
 -- file Spec.hs
 import Test.Hspec
 import Control.Exception (evaluate)
+import qualified Numeric
+
 
 main :: IO ()
 main = hspec $ do
+  describe "myTests" $ do
+    it "checks Numeric.readFloat" $ do
+      Numeric.readFloat "1e1000000" `shouldBe` ( [(0,"")]:: [(a0,String)] )
+
   describe "Prelude.head" $ do
     it "returns the first element of a list" $ do
       head [23 ..] `shouldBe` (23 :: Int)
